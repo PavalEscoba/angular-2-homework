1568e0dde1efb93cf0580f4bc044c17a2df8c67e
PavalEscoba <paval.escoba@gmail.com>
2016-10-21 21:45:48 +0300

    Made using Angular

---
+++ js/stringController.js
@@ -0,0 +1,18 @@
+var app = angular.module("myApp", []);
+
+app.controller("stringController", function($scope){
+	function isNumeric(n){
+  		return !isNaN(parseFloat(n)) && isFinite(n);//праверка на лічбу
+	};
+	$scope.cuting = function(){
+		$scope.result ="";
+		if(!isNumeric($scope.number) || $scope.number<0){
+			alert("Enter positive number")
+		}
+		else{
+		$scope.result = $scope.string.slice(0,$scope.number);
+		}
+	}
+
+});
+
